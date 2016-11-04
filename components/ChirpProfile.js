29143d4bfdcab260144107b6a345f57e8f304ff8
Young Mi Kim <ymkim23@gmail.com>
2016-11-04 16:42:18 -0400

    working on profile

--- components/ChirpProfile.js
+++ components/ChirpProfile.js
@@ -3,8 +3,29 @@ import React from 'react'
 class ChirpProfile extends React.Component {
   constructor(props) {
     super(props)
-    //this.state
+    this.profileData = this.profileData.bind(this)
+    this.state= {
+      username:'',
+      email:''
+    }
   }
+
+  profileData() {
+    var data = new FormData()
+    data.append('username', this.state.username)
+    data.append('email', this.state.email)
+
+    fetch('https://polar-sea-81260.herokuapp.com', {
+      method: 'Get',
+      body: data
+    })
+    .then(response => response.json())
+    .then(function(response){
+        console.log(response)
+    })
+
+  }
+
 
   render() {
     return <div className="col-sm-3">
@@ -14,8 +35,8 @@ class ChirpProfile extends React.Component {
           <img src="img/logo.png" alt="profile pic" />
           <div className="row">
             <div className="col-sm-12 text-center">
-              <h5>Name</h5>
-              <p>Email</p>
+              <h5>{response.username}</h5>
+              <p>{response.email}</p>
               <br />
             </div>
           </div>
